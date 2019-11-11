1627e2c40cda41f4d8cdab1c58391bdefea613a9
Dante S Bortone <dbortone@email.unc.edu>
2019-11-11 11:41:54 -0500

    Changed file names.

---
+++ R/PrePostMiXCR.R
@@ -0,0 +1,10 @@
+#' StarSalmon: Functions for writing commands and post processing the MiXCR pipeline.
+#'
+#' Given a sample data matrix that indicate the sample name and file paths post_process_mixcr will 
+#' run the diversity calculation metrics on those MiXCR output files.
+#' 
+#' @import magrittr data.table parallel binfotron checkmate
+#' 
+#' @keywords internal
+"_PACKAGE"
+#> [1] "_PACKAGE"
